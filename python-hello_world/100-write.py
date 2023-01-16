9fb426b0df40d16782633297c3bd2bf6287af2c3
ali segueg <alisegueg123@outllok.fr>
2023-01-16 16:32:00 +0100

    task 100

--- python-hello_world/100-write.py
+++ python-hello_world/100-write.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 import sys
-sys.stderr.write("et cette œuvre d'art est utile - Dora Korpar, 2015-10-19\n")
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
 sys.exit(1)
