bd3896f951f8b98b01be5f32b905177f582358c3
ali segueg <alisegueg123@outllok.fr>
2023-01-16 13:24:45 +0100

    task6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,8 @@ for i in range(10):
     for j in range(10):
         if i < j:
             k = str(i) + str(j)
+            h = int(k)
             if k != "89":
-                print("{}".format(k), end=", ")
+                print("{}".format(h), end=", ")
             else:
-                print("{}".format(k), end=" ")
+                print("{}".format(h), end=" ")
