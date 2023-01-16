c77fc37cde57f2530e9b1934907c29a4849dfbb1
ali segueg <alisegueg123@outllok.fr>
2023-01-16 11:09:53 +0100

    task3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
-alphabet = [chr(i) for i in range(97, 123)]
-for i in alphabet:
-    print(i, end='')
+#!/usr/bin/python3
+for lettre in range(97, 123):
+    if lettre != 101 and lettre != 113:
+        print("{:c}".format(lettre), end='')
