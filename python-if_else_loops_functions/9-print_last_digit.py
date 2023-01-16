95d28f354b95b7b426eada269e4406432e161caf
ali segueg <alisegueg123@outllok.fr>
2023-01-16 14:20:34 +0100

    task9

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-        modulo_du_posetif = (number*-1) % 10
-        print(modulo_du_posetif)
-        return modulo_du_posetif
-    else:
-        print(number % 10)
+        number *= -1
+    number %= 10
+    print(number, end="")
+    return(number)
