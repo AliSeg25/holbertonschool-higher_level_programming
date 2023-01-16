cd60fc668d71d55256d388735da1bdca8a858e88
ali segueg <alisegueg123@outllok.fr>
2023-01-16 11:37:30 +0100

    task5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 for i in range(100):
     if i != 99:
-        print("{:d}, ".format(i), end="")
-    else:
-        print(i)
+        if i < 10:
+            print("0{:d}, ".format(i), end="")
+        elif i >= 10:
+            print("{:d}, ".format(i), end="")
+        else:
+            print(i)
