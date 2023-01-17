5d98d5095d9df9aade960f8bf4700ba40844dc9a
ali segueg <alisegueg123@outllok.fr>
2023-01-17 08:44:15 +0100

    calculatrice

--- python-import_modules/calculator_1.py
+++ python-import_modules/calculator_1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def add(a, b):
+""""def add(a, b):
     return a + b
 
 def sub(a, b):
@@ -10,4 +10,19 @@ def mul(a, b):
     return a * b
 
 def div(a, b): 
-    return a / b
+    return a / b"""
+
+class Calculation:
+    def add(self, a, b):
+        return a + b
+
+    def sub(self, a, b):
+        return a - b
+
+    def mul(self, a, b):
+        return a * b
+
+    def div(self, a, b): 
+        return a / b
+
+calc = Calculation()
