41893934024b7696c2429e0ed97520ac9e97135e
ali segueg <alisegueg123@outllok.fr>
2023-01-17 09:38:38 +0100

    Task5

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+import importlib.util
+import pkgutil
+
+if __name__ == "__main__":
+    hidden_4 = importlib.util.find_spec("hidden_4")
+    hidden_4 = importlib.util.module_from_spec(hidden_4)
+    hidden_4.__loader__.exec_module(hidden_4)
+    names = [name for name in dir(hidden_4) if not name.startswith("__")]
+    names.sort()
+    for name in names:
+        print(name)
