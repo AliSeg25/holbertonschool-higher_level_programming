1a5de52225e746a35df313745d49c09bb12ba0c1
ali segueg <alisegueg123@outllok.fr>
2023-01-20 08:52:27 +0100

    -a

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-import dis
-import marshal
+#!/usr/bin/python3
+import hidden_4
 
-def hidden_discovery(filepath):
-    with open(filepath, 'rb') as f:
-        magic = f.read(4)
-        moddate = f.read(4)
-        code = marshal.load(f)
-
-    for i in dis.get_instructions(code):
-        if i.argrepr.startswith("'") and not i.argrepr.startswith("'_"):
-            print(i.argrepr.strip("'"))
-
-if __name__ == '__main__':
-    hidden_discovery("hidden_4.pyc")
+array = dir(hidden_4)
+if __name__ == "__main__":
+    for i in array:
+        if i[:2] != '__':
+            print(i)
