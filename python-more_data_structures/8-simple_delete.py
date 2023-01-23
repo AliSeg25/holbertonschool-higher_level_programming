b1967a798fc24af9294db70739605f4336cc6ceb
ali segueg <alisegueg123@outllok.fr>
2023-01-23 15:19:55 +0100

    task8

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    a_dictionary.pop(key)
-    return a_dictionary
+    if key in a_dictionary:
+        a_dictionary.pop(key)
+        return a_dictionary
+    else:
+        return a_dictionary
+print(simple_delete({'a': 1, 'b': 2, 'c': 3}, 'f'))
