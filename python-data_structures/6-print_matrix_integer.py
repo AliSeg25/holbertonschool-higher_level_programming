c6e3ed8e585b2ff9dc4dd193ba2c9bfedfc999a6
ali segueg <alisegueg123@outllok.fr>
2023-01-23 11:11:03 +0100

    task6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix is None or len(matrix) == 0:
+       if matrix == [] or matrix is None:
         print()
         return None
     for i in range(len(matrix)):
