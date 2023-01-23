6379a6b51e69d1d4c01b2268b8fed2809f0fa7d2
ali segueg <alisegueg123@outllok.fr>
2023-01-23 10:24:01 +0100

    task4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    nv_list = my_list[:]
+    if idx < 0 or idx > len(nv_list)-1:
+        return nv_list
+    else:
+        nv_list[idx] = element
+        return nv_list
