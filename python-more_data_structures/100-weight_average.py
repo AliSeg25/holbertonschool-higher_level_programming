6749be7dba7898b48809b949115ab86620065d4c
ali segueg <alisegueg123@outllok.fr>
2023-01-23 18:47:47 +0100

    tas13

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list == []:
+        return 0
+    
+    total = [sum(x) for x in my_list]
+    len = [len(x) for x in my_list]
+
+    return sum(total) / sum(len)
