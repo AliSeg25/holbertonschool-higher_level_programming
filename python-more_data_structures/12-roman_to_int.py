fafe789228cd5146a8937ccdea2485a6405e8122
ali segueg <alisegueg123@outllok.fr>
2023-01-23 16:20:54 +0100

    task12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    res = 0
+    dict ={
+    "I" : 1,
+    "V" : 5,
+    "X" : 10,
+    "L" : 50,
+    "C" : 100,
+    "D" : 500,
+    "M" : 1000}
+    for i in roman_string:
+        for j in dict:
+            if j == i:
+                res += dict[j]
+    return res
