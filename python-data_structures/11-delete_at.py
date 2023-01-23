7acb220ff3a51df4927ba2f9a9769b3da5b80886
ali segueg <alisegueg123@outllok.fr>
2023-01-23 12:23:46 +0100

    11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,9 +2,10 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0:
         return my_list
-    if idx > len(my_list):
+    elif idx > len(my_list):
         return my_list
-    if idx == len(my_list):
-        idx -= 1
-    del my_list[idx]
-    return my_list
+    elif idx == len(my_list):
+        return my_list
+    else:
+        del my_list[idx]
+        return my_list
