a4f9091f58c72fbd6ea6a9b093a2a0797c80f198
ali segueg <alisegueg123@outllok.fr>
2023-01-23 16:29:51 +0100

    task12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -13,14 +13,14 @@ def roman_to_int(roman_string):
     else:
         res = 0
         dict ={
-        "IV" : 4,
-        "I" : 1,
-        "V" : 5,
-        "X" : 10,
-        "L" : 50,
-        "C" : 100,
-        "D" : 500,
-        "M" : 1000}
+        "IV": 4,
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000}
         for i in roman_string:
             for j in dict:
                 if j == i:
