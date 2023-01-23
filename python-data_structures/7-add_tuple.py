a134b3a646f69bdca49a871dc40d38b378a9e0f0
ali segueg <alisegueg123@outllok.fr>
2023-01-23 11:42:27 +0100

    task7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -4,5 +4,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a = tuple_a + (0, 0)
     if len(tuple_b) < 2:
         tuple_b = tuple_b + (0, 0)
-    nv_tuple = tuple(map(sum, zip(tuple_a, tuple_b)))
-    return nv_tuple
+    return tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
