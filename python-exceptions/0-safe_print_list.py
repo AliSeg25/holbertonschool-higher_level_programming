59b1387b40e4e627360907c21694a328297603ff
ali segueg <alisegueg123@outllok.fr>
2023-01-24 10:16:12 +0100

    task0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    nb = 0
+
+    try:
+        for i in range(x):
+            print("{:}".format(my_list[i]), end="")
+
+    except:
+        print("x est superieur a la chaine")
+
+    print()
+    return nb
