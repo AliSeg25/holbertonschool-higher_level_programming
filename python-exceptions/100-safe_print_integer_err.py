bd224f322a19894d0d78ca5843c99a8203210dd4
ali segueg <alisegueg123@outllok.fr>
2023-01-25 17:47:22 +0100

    task7

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+import sys
 def safe_print_integer_err(value):
     try:
             if isinstance(value, int):
-            print("{:d}".format(value))
-            return True
+                print("{:d}".format(value))
+                return True
     except Exception as e:
             print("Exception: {}".format(e), file=sys.stderr)
             return False
