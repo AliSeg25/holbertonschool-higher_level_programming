e022586f7217aa89f98bca3821ec91dc9c149b71
ali segueg <alisegueg123@outllok.fr>
2023-01-25 16:02:06 +0100

    task6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -5,6 +5,8 @@ def print_matrix_integer(matrix=[[]]):
         return None
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end=" " if j < len(matrix[i])-1 else "")
+
+            print("{:d}".format(matrix[i][j]), end=" ")
+            
             if j == len(matrix[i])-1:
                 print()
