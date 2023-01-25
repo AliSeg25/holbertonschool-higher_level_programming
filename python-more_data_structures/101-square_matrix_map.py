fe29ce14bb7df5de5d0730f7bf6048c207ba9a66
ali segueg <alisegueg123@outllok.fr>
2023-01-25 10:54:31 +0100

    task 13

--- python-more_data_structures/101-square_matrix_map.py
+++ python-more_data_structures/101-square_matrix_map.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def square_matrix_map(matrix=[]):
+    nv_matrix = []
+    for i in range(len(matrix)):
+        nv_matrix.append(list(map(lambda x: x ** 2, matrix[i])))
+    return nv_matrix
