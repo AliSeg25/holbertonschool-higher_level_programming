d397b1ebf08f8d244e5db40d6a2bddc02a2f9c09
ali segueg <alisegueg123@outllok.fr>
2023-01-25 11:24:26 +0100

    task FIN

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    if not a_dictionary:
+        return a_dictionary
+    for i, j in list(a_dictionary.items()):
+        if j == value:
+            del a_dictionary[i]
+    return a_dictionary
