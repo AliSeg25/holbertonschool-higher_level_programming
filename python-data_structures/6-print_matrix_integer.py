f571880c6b7a05c68f9412c27544c28f0dd449bd
ali segueg <alisegueg123@outllok.fr>
2023-01-25 16:33:43 +0100

    task6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -10,13 +10,3 @@ def print_matrix_integer(matrix=[[]]):
             
             """if j == len(matrix[i])-1:
                 print()"""
-            
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
