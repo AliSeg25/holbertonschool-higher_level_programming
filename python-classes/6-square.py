eb0e36a5006145e5b3440f2215510a3fb4c30ab6
ali segueg <alisegueg123@outllok.fr>
2023-01-30 17:38:02 +0100

    task6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
 """This module contains a class called Square"""
 
-
 class Square:
-    """Initialize the square with size """
-
     def __init__(self, size=0, position=(0, 0)):
         self.__size = size
         self.__position = position
 
     @property
     def size(self):
-
-        """Initialize the square with size """
+        """Return the size of the square"""
         return self.__size
 
     @size.setter
     def size(self, value):
-
-        """Veriification de la valeur"""
+        """Verify the value of the size"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -27,45 +22,33 @@ class Square:
 
     @property
     def position(self):
-
-        """Initialize the square with size """
+        """Return the position of the square"""
         return self.__position
 
     @position.setter
     def position(self, value):
-
-        """Veriification de la valeur"""
+        """Verify the value of the position"""
         if not isinstance(value, tuple) or len(value) != 2:
-            raise TypeError("position must be a tuple of\
-                             2 positive integers")
-
-        for i in range(len(self.position)):
-
-            if not isinstance(value[i], int):
-                raise TypeError("position must be a tuple of\
-                                 2 positive integers")
-
-            if self.position[i] < 0:
-                raise TypeError("position must be a tuple of\
-                                 2 positive integers")
-
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not all(map(lambda x: isinstance(x, int) and x >= 0, value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
-
-        """ Return the area of the square """
-        self.size **= 2
-        return self.size
+        """Return the area of the square"""
+        return self.size ** 2
 
     def my_print(self):
-
-        """Print #"""
+        """Print the square"""
         if self.size == 0:
             print()
-        else:
-            for i in range(self.size):
-                for j in range(self.position[0]):
-                    print("_", end="")
-                for k in range(self.size):
-                    print("#", end="")
-                print()
+            return
+        for i in range(self.position[1]):
+            print()
+        for i in range(self.size):
+            print(" " * self.position[0] + "#" * self.size)
+
+try:
+    my_square = Square(3, "Position")
+except Exception as e:
+    print(e)
