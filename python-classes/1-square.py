77df5e67654bf24f77143afe222a068c731d0522
ali segueg <alisegueg123@outllok.fr>
2023-01-30 13:08:11 +0100

    task1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Script that assigns a private instance attribute size to class Square"""
+
+
 class Square:
     """Class that defines a square with a private instance attribute size"""
 
