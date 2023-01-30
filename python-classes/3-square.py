e3cea00ab4ad3f073509923911de7313785a0162
ali segueg <alisegueg123@outllok.fr>
2023-01-30 14:38:48 +0100

    task3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""This module contains a class called Square"""
+
+
+class Square:
+    """ Initialize the square with size """
+
+    def __init__(self, size=0):
+        """ Initialize the square with size """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self._size = size
+
+    def area(self):
+        """ Return the area of the square """
+        return self._size ** 2
