68ff76a2e89ad11cb99890cfd0b1f49dc9fb5314
ali segueg <alisegueg123@outllok.fr>
2023-01-30 12:59:21 +0100

    task0

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 """Write a class Square"""
 
+
+class Square:
+    """This is the Square class"""
+    def __init__(self, size):
+        """This is the instantiation of size"""
+        self.__size = size
