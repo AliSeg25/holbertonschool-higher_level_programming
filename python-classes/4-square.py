48651abd0e81e3854dbb3e7e1c72f6838b37bffc
ali segueg <alisegueg123@outllok.fr>
2023-01-30 15:27:55 +0100

    task4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""This module contains a class called Square"""
+
+
+class Square:
+    """Initialize the square with size """
+
+    def __init__(self, size=0):
+        """ Initialize the square with size """
+        self.__size = size
+
+    @property
+    def size(self):
+
+        """ Initialize the square with size """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+
+        """Veriification de la valeur"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+
+        """ Return the area of the square """
+        return self.size ** 2
