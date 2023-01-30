e4aa97a7ae39021f81ab1d1f6867c6d9e70411f2
ali segueg <alisegueg123@outllok.fr>
2023-01-30 14:14:18 +0100

    task2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+""" 2-square.py: Square class with private instance attribute size """
+
+
+class Square:
+    """ Square class with private instance attribute size """
+
+    def __init__(self, size=0):
+        """ Initialize the square with size """
+        
+        if not isinstance(size, int):
+            self.__size = size
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
