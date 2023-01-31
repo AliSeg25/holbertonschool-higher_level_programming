567ab431f2092892f8fc596b7a604a33b2bd6dbb
ali segueg <alisegueg123@outllok.fr>
2023-01-31 08:57:40 +0100

    task0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""Add integer module."""
+
+
+def add_integer(a, b=98):
+    """
+    Add integer function.
+    @a: integer or float to be added.
+    @b: integer or float to be added (default set to 98).
+    Returns the result of the sum.
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    
+    return int(a + b)
