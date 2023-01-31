efde6fbb46651a9c47e09cab5b95dd0387a19ebd
ali segueg <alisegueg123@outllok.fr>
2023-01-31 13:42:06 +0100

    task2

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -8,24 +8,17 @@ def matrix_divided(matrix, div):
     @matrix: matrix to be divided composed of ints ot floats
     @div: integer that will divide the matrix
     """
-    msg = 'matrix must be a matrix (list of lists) of integers/floats'
-    if not isinstance(matrix, list):
-        raise TypeError(msg)
-    n_mat = matrix.copy()
+
+    if not all(isinstance(el, list) for el in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    if not all(len(el) == len(matrix[0]) for el in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if type(div) not in [int, float]:
+        raise TypeError("div must be a number")
+
     if div == 0:
-        raise ZeroDivisionError('division by zero')
-    if not isinstance(div, int) and not isinstance(div, float):
-        raise TypeError('div must be a number')
-    for i in range(len(matrix)):
-        if not isinstance(matrix[i], list):
-            raise TypeError(msg)
-        mat_len = len(matrix[0])
-        n_mat[i] = matrix[i].copy()
-        if len(matrix[i]) != mat_len:
-            raise TypeError('Each row of the matrix must have the same size')
-        for j in range(len(matrix[i])):
-            if not isinstance(matrix[i][j], int)\
-                    and not isinstance(matrix[i][j], float):
-                raise TypeError(msg)
-            n_mat[i][j] = round(matrix[i][j] / div, 2)
-    return n_mat
+        raise ZeroDivisionError("division by zero")
+
+    return [[round(el / div, 2) for el in liste] for liste in matrix]
