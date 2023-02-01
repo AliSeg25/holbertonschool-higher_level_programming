e6c3a5dc4a00d5713afc658698651bf4ae4250a2
ali segueg <alisegueg123@outllok.fr>
2023-02-01 10:54:41 +0100

    task1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,1 +1,47 @@
 #!/usr/bin/python3
+"""Define a class"""
+
+
+class Rectangle:
+    """Define a class Rectancle"""
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Return private width"""
+
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        """Checks the Values"""
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        
+        if value < 0:
+            raise TypeError("width must be >= 0")
+
+        self.__height = value
+        
+
+    @property
+    def height(self):
+        """Return private height"""
+
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        """checks the values"""
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        
+        if value < 0:
+            raise TypeError("height must be >= 0")
+        
+        self.__height = value
