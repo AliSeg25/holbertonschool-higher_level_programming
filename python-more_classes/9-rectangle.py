ed0b7ae652c09aedfc63051c6dba4c21537eb329
ali segueg <alisegueg123@outllok.fr>
2023-02-01 17:12:13 +0100

    task9

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -24,7 +24,7 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        return cls(size, size)
+        cls(size, size)
 
     def __init__(self, width=0, height=0):
         self.width = width
