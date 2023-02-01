5cde3aa21e9e607a03cbdac313174950c2bbc5b0
ali segueg <alisegueg123@outllok.fr>
2023-02-01 12:57:42 +0100

    task

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -17,28 +17,3 @@ def add_integer(a, b=98):
         raise TypeError("b must be an integer")
 
     return int(a) + (b)
-#!/usr/bin/python3
-"""Write a function that divides all elements of a matrix.
-
-
-def matrix_divided(matrix, div):
-    """
-    """
-    Matrix division function
-    @matrix: matrix to be divided composed of ints ot floats
-    @div: integer that will divide the matrix
-    """
-    """
-    if not all(isinstance(el, list) for el in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-
-    if not all(len(el) == len(matrix[0]) for el in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
-
-    if type(div) not in [int, float]:
-        raise TypeError("div must be a number")
-
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
-
-    return [[round(el / div, 2) for el in liste] for liste in matrix]"""
