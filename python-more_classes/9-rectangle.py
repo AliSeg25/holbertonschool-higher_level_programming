8f28ab672bc35f6057518042a450092581a58fea
ali segueg <alisegueg123@outllok.fr>
2023-02-01 17:34:36 +0100

    task

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -24,6 +24,8 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
+        if size < 0:
+            raise ValueError("width must be >= 0")
         return cls(size, size)
 
     def __init__(self, width=0, height=0):
