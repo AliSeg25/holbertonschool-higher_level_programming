af7f9a26de60262ec4911d53b638b2d5092de8e6
ali segueg <alisegueg123@outllok.fr>
2023-02-01 17:40:20 +0100

    task

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -45,7 +45,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
 
         if value < 0:
-            raise TypeError("width must be >= 0")
+            raise ValueError("width must be >= 0")
 
         self.__width = value
 
