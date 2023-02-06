b54a6582306bd2137a5580f9f4b1ab5c911ea63a
ali segueg <alisegueg123@outllok.fr>
2023-02-06 19:16:10 +0100

    task7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -8,7 +8,7 @@ class BaseGeometry:
     def area(self):
         """Raises an exception"""
         raise Exception('area() is not implemented')
-    
+
     def integer_validator(self, name, value):
         """Fonction Raises an exception"""
 
