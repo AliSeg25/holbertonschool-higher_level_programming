8a75fc52ba6f35d3d461342bc4129df1ce0d24eb
ali segueg <alisegueg123@outllok.fr>
2023-02-06 14:48:57 +0100

    task4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,5 +5,5 @@
 def inherits_from(obj, a_class):
     """Write a function that returns True if the object is an instance of a class that inherited """
 
-    return (type(obj) is not a_class\
-             and isinstance(obj, a_class))
+    return (type(obj) is not a_class
+            and isinstance(obj, a_class))
