e66ed0775a892123cdf7a9dcb02d607cc0ad1956
ali segueg <alisegueg123@outllok.fr>
2023-02-06 19:04:00 +0100

    task6

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""5. Geometry module"""
+
+
+class BaseGeometry:
+    """class empty"""
+
+    def area(self):
+        """Raises an exception"""
+        raise Exception('area() is not implemented')
