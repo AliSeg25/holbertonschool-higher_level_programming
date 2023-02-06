acda9d366c1df1dfe0a9f8481a2f243576e819bb
ali segueg <alisegueg123@outllok.fr>
2023-02-06 13:56:50 +0100

    task1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""class and function"""
+
+
+class MyList(list):
+    """Created class inherits list"""
+
+    def __init__(self):
+        super().__init__()
+
+    def print_sorted(self):
+        """Print self"""
+
+        print(sorted(self))
