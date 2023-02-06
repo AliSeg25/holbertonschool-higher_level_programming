f9acfc89bba331be8d6dff4f9263f23cff1ddded
ali segueg <alisegueg123@outllok.fr>
2023-02-06 19:28:40 +0100

    task8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+"""5. Geometry module"""
+
+
+class BaseGeometry:
+    """class empty"""
+
+    def area(self):
+        """Raises an exception"""
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """Fonction Raises an exception"""
+
+        if not type(value) is int:
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
+
+class Rectangle(BaseGeometry):
+    """class rectancle inherits basegeometry"""
+
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+        @property
+        def width(self):
+            return self.__width
+        
+        @property
+        def height(self):
+            return height.__width
