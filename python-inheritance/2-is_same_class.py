1b1d9ff0ffca245ccdd5142445cd77a904ec9ce6
ali segueg <alisegueg123@outllok.fr>
2023-02-06 14:13:43 +0100

    task2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+""" function is_same_class"""
+
+
+def is_same_class(obj, a_class):
+    """Return True type(obj) ==  a_class"""
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
