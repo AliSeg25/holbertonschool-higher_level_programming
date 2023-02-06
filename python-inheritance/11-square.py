c3215f471b2ea0457cfea53db819ada1f79710fe
ali segueg <alisegueg123@outllok.fr>
2023-02-06 20:15:32 +0100

    task11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""Class Square"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class Suare"""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """Description rectangle"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -9,6 +9,7 @@ class BaseGeometry:
         """Raises an exception"""
         raise Exception('area() is not implemented')
 
+
     def integer_validator(self, name, value):
         """Fonction Raises an exception"""
 
