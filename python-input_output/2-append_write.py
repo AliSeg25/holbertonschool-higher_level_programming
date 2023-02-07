4ab1eac56b838b2367e15a603a68bfb27a181bb3
ali segueg <alisegueg123@outllok.fr>
2023-02-07 10:53:38 +0100

    task2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""Function append_write"""
+
+
+def append_write(filename="", text=""):
+    with open(filename, "a") as fichier:
+        fichier.write(text)
+        return text
