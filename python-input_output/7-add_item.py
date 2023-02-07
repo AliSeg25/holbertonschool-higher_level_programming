bea905b0106c8144f94fd2a7cd3e504e7e8b29e2
ali segueg <alisegueg123@outllok.fr>
2023-02-07 15:39:29 +0100

    task7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 
-load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
 import sys
 
-arg = sys.argv[1:]
+file = "add_item.json"
 
-print(arg)
+arg = sys.argv[1:]
+datajson = load_from_json_file(arg)
+with open(file, "w") as file:
+    file.write(datajson)
+
+print(datajson)
