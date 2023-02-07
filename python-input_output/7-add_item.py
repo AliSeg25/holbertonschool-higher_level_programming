bdd051f421c3c3252b9b1c41669fc72d2a2383c7
ali segueg <alisegueg123@outllok.fr>
2023-02-07 15:49:27 +0100

    task7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,10 +7,13 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 import sys
 
 file = "add_item.json"
-
 arg = sys.argv[1:]
-json = load_from_json_file(arg)
-with open(file, "w") as file:
-    file.write(json)
 
-print(json)
+try:
+    obj = load_from_json_file(file)
+except:
+    obj = [] 
+
+
+obj += arg
+save_to_json_file(obj, file)
