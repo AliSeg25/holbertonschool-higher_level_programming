6856eaf688debdb35f17a48013c2ef38f92744fa
ali segueg <alisegueg123@outllok.fr>
2023-02-07 10:36:57 +0100

    task0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,5 +6,6 @@ def read_file(filename=""):
     """Open file simple reading"""
 
     with open(filename, "r") as f:
-        fichier = f.read()
-    print(fichier)
+        for ligne in f
+        #fichier = f.read()
+            print(ligne)
