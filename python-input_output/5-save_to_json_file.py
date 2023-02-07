0da3d43ad2a4438d45331dda2310cc9e251ea0a0
ali segueg <alisegueg123@outllok.fr>
2023-02-07 13:06:46 +0100

    task5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -10,9 +10,22 @@ def save_to_json_file(my_obj, filename):
     try:
             data = json.dumps(my_obj)
     except:
-        raise PermissionError("[TypeError] Object of type set is not JSON serializable")
+        raise TypeError("Object of type set is not JSON serializable")
     try:
         with open(filename, "w") as fichier:
             fichier.write(data)
     except:
         raise PermissionError("[Errno 13] Permission denied: '{:}'".format(filename))
+try:
+    filemame = "file_7"
+    data = { 1, 2, 3 }
+    save_to_json_file(data, filemame)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    filemame = "no_perm/file_7"
+    data = [1, 2, 3]
+    save_to_json_file(data, filemame)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
