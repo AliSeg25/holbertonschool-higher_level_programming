fa68f0e77a49dec164a00c2d9c4c68eff3ea7fab
ali segueg <alisegueg123@outllok.fr>
2023-02-07 13:04:10 +0100

    task5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -16,17 +16,3 @@ def save_to_json_file(my_obj, filename):
             fichier.write(data)
     except:
         raise PermissionError("[Errno 13] Permission denied: '{:}'".format(filename))
-
-try:
-    filemame = "file_7"
-    data = { 1, 2, 3 }
-    save_to_json_file(data, filemame)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
-
-try:
-    filemame = "no_perm/file_7"
-    data = [1, 2, 3]
-    save_to_json_file(data, filemame)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
