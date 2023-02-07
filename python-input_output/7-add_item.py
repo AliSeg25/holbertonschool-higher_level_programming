22560c3c1aa781a197463626236c032113f3302f
ali segueg <alisegueg123@outllok.fr>
2023-02-07 16:54:53 +0100

    task0

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
+import sys
+import json
 
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-
-import sys
-
-file = "add_item.json"
-arg = sys.argv[1:]
-
 try:
-    liste = load_from_json_file(file)
-    liste =+ arg
+    my_list = load_from_json_file("add_item.json")
 except:
-    pass
+    my_list = []
 
+for i in range(1, len(sys.argv)):
+    my_list.append(sys.argv[i])
+
+save_to_json_file(my_list, "add_item.json")
