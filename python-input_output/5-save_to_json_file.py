751caec6b024efb47e2ef130749211212ce0e2f5
ali segueg <alisegueg123@outllok.fr>
2023-02-07 12:38:53 +0100

    task5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Write a function that returns an object"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Transform a data into file json and write il to file"""
+
+    data = json.dumps(my_obj)
+    with open(filename, "w") as fichier:
+        fichier.write(data)
