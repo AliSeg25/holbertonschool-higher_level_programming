7f6bdafac9583ace8e686ef16de3741b47ed3b9b
ali segueg <alisegueg123@outllok.fr>
2023-02-07 13:00:01 +0100

    task5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -7,8 +7,10 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """Transform a data into file json and write il to file"""
-
-    data = json.dumps(my_obj)
+    try:
+            data = json.dumps(my_obj)
+    except:
+         raise PermissionError("[Errno 13] Permission denied: 'file_7'")
     try:
         with open(filename, "w") as fichier:
             fichier.write(data)
