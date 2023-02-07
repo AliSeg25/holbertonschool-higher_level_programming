55e2a77f2e982d9a23990fce826e367dbed0b0d9
ali segueg <alisegueg123@outllok.fr>
2023-02-07 12:49:48 +0100

    task5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -9,5 +9,8 @@ def save_to_json_file(my_obj, filename):
     """Transform a data into file json and write il to file"""
 
     data = json.dumps(my_obj)
-    with open(filename, "w") as fichier:
-        fichier.write(data)
+    try:
+        with open(filename, "w") as fichier:
+            fichier.write(data)
+    except:
+        raise PermissionError("Permission denied: '{:}'".format(filename))
