d686c2d458f3c3ff62cd0a6db76cfefc1128d57e
ali segueg <alisegueg123@outllok.fr>
2023-02-13 18:47:29 +0100

    SQuarze

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -142,5 +142,5 @@ class Rectangle(Base):
     def to_dictionary(self):
         """Returns the dictionary representation of the square instance"""
 
-        return {'id': self.id, 'width': self.width,\
-                 'height': self.height, 'x': self.x, 'y': self.y}
+        return {'id': self.id, 'width': self.width,
+                'height': self.height, 'x': self.x, 'y': self.y}
