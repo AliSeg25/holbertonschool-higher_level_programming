c7e39dc494f32ebb2b8edb98d88a89da66f6061e
ali segueg <alisegueg123@outllok.fr>
2023-02-13 08:47:37 +0100

    creation projet

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -1,1 +1,27 @@
-#!/usr/bin/python3
+def modify_list(lst):
+    print("Avant modification :", lst)
+    lst.append(1)
+    print("Après modification :", lst)
+
+def modify_string(string):
+    print("Avant modification :", string)
+    string = string + " modifié"
+    print("Après modification :", string)
+
+my_list = [2, 3, 4]
+modify_list(my_list)
+print("Liste après la fonction :", my_list)
+
+my_string = "Chaine de caractères"
+modify_string(my_string)
+print("Chaine de caractères après la fonction :", my_string)
+
+#Objet Mutable 
+#Avant modification : [2, 3, 4]
+#Après modification : [2, 3, 4, 1]
+#Liste après la fonction : [2, 3, 4, 1]
+
+#Objet Immutable
+#Avant modification : Chaine de caractères
+#Après modification : Chaine de caractères modifié
+#Chaine de caractères après la fonction : Chaine de caractères
