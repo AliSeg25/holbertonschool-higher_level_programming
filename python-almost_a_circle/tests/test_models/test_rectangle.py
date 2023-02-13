4e870bb3d90c497b9550e394694770a9c9420873
ali segueg <alisegueg123@outllok.fr>
2023-02-13 11:47:03 +0100

    rectangle

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,1 +1,22 @@
+import unittest
 
+from models.rectangle import Rectangle
+from models.base import Base
+import models.rectangle
+import models.base
+
+
+#Base = __import__('models.base').Base
+#Rectancle = __import__('models.rectangle').Rectangle
+
+class TestRectangle(unittest.TestCase):
+    def test_init(self):
+        r = Rectangle(10, 20, 30, 40, 50)
+        self.assertEqual(r.width, 10)
+        self.assertEqual(r.height, 20)
+        self.assertEqual(r.x, 30)
+        self.assertEqual(r.y, 40)
+        self.assertEqual(r.id, 50)
+        
+if __name__ == '__main__':
+    unittest.main()
