593e481975343361df5a1e4117404365edf80ca5
ali segueg <alisegueg123@outllok.fr>
2023-02-13 11:35:31 +0100

    Recherche

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,3 +1,60 @@
 #!/usr/bin/python3
-def addition(a, b):
-    return a + b
+"""Class Rectangle"""
+
+
+#from models.base import Base
+
+Base = __import__('base').Base
+
+
+
+class Rectangle(Base):
+    """Class rertagle qui herite de la class base"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id=id)
+    
+
+    @property
+    def width(self):
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        self.__width = value
+    
+    @property
+    def height(self):
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        self.__height = value
+    
+    @property
+    def x(self):
+        return self.__x
+    
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+    
+    @y.setter
+    def y(self, value):
+        self.__y = value
+    
+    @property
+    def id(self):
+        return self.__id
+    
+    @id.setter
+    def id(self, value):
+        self.__id = value
