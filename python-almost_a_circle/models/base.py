7ee936bc429c775d8d341522efdd069db5cd09eb
ali segueg <alisegueg123@outllok.fr>
2023-02-14 12:26:37 +0100

    task to_json string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -60,13 +60,9 @@ class Base:
 
         return liste_instance
 
-
     def __init__(self, id=None):
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-    
-
-
