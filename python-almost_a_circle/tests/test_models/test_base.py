2cdd304f182b217912b6e17dc8b50f7c5835400c
ali segueg <alisegueg123@outllok.fr>
2023-02-14 09:52:30 +0100

    task to_json string

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -2,6 +2,8 @@ import unittest
 from models.rectangle import Rectangle
 from models.base import Base
 from models.square import Square
+import os
+import json
 
 """a"""
 
@@ -37,7 +39,28 @@ class TestBase(unittest.TestCase):
         none_json_str = Base.to_json_string(None)
         self.assertEqual(none_json_str, "[]")
 
-        
+
+    def test_save_to_file(self):
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        Rectangle.save_to_file([r1, r2])
+
+        with open("Rectangle.json", "r") as file:
+            json_content = file.read()
+            expected_content = json.dumps([r1.to_dictionary(), r2.to_dictionary()])
+            self.assertEqual(json_content, expected_content)
+
+        os.remove("Rectangle.json")
+    
+    def test_from_json_string(self):
+        # Test avec une liste de dictionnaires non vide
+        list_input = [
+            {'id': 89, 'width': 10, 'height': 4},
+            {'id': 7, 'width': 1, 'height': 7}
+        ]
+        json_list_input = Rectangle.to_json_string(list_input)
+        list_output = Rectangle.from_json_string(json_list_input)
+        self.assertEqual(list_output, list_input)
 
 if __name__ == '__main__':
     unittest.main()
