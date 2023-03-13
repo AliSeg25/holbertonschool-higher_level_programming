91f256f9cde1ff3702630acda8441e3dea43d471
ali segueg <alisegueg123@outllok.fr>
2023-03-13 20:46:50 +0100

    model

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines the State class and links to the states table in the database"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+"""Base = declarative_base() crée une instance de la classe declarative_base de SQLAlchemy
+qui est utilisée pour définir des classes de modèles en tant que classe de base.
+Cette classe fournit une variété de fonctionnalités utiles pour définir et interagir
+avec des objets de base de données."""
+Base = declarative_base()
+
+class State(Base):
+
+    """La ligne de code __tablename__ = 'states' 
+    est utilisée pour lier la classe Python à la table MySQL"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
