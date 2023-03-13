3e9a3e694ba61752d434a6e8a16a3d3f5f95dc18
segueg Ali <alisegueg2501@gmail.com>
2023-03-13 11:54:13 +0100

    task5

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,2 +1,5 @@
--- Creates a table "first_table" in the current database.
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256)
+-- creates a table called first_table
+CREATE TABLE IF NOT EXISTS first_table (
+       id INT,
+       name VARCHAR(256)
+);
