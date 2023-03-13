8aabf4cc90ecdb44d5c9677e732817e34326648b
segueg Ali <alisegueg2501@gmail.com>
2023-03-13 17:20:55 +0100

    task0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,40 @@
-#!/usr/bin/python3
-'''States listing module'''
+#!/usr/bin/env python3
+
+"""
+Lists all states from the database hbtn_0e_0_usa sorted in ascending order by states.id
+"""
+
 import MySQLdb
-from sys import argv
+import sys
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host='localhost', user=argv[1], passwd=argv[2],
-                         db=argv[3])
-    st = db.cursor()
-    st.execute("""SELECT `id`, `name` FROM states ORDER BY `id`;""")
-    res = st.fetchall()
-    for i in res:
-        print(i)
-    db.close() 
+    # Get command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=database)
+
+    # Create a cursor object allows to execute
+    cursor = db.cursor()
+
+    #Cette ligne exécute une requête SQL qui sélectionne 
+    # toutes les lignes de la table "states" dans la base de données 
+    # triées par ordre croissant de leur id.
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Ces lignes extraient toutes les lignes de résultats 
+    # de la requête et les affichent à l'écran.
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    # Close cursor and database
+    cursor.close()
+    db.close()
