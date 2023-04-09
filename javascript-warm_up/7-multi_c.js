12fbc643911ee99a345b78eb81e97229e3bac474
Your Name <you@example.com>
2023-04-09 09:49:57 +0200

    task7

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const nb = Number(process.argv[2]);
+
+if (isNaN(nb)) {
+  console.log("Missing number of occurrences");
+} else {
+  for (let i = 0; i < nb; i++) {
+    console.log("C is fun");
+  }
+}
