961cbd6e94a92dd3f62664e35773e6614fb62515
Your Name <you@example.com>
2023-04-09 09:41:31 +0200

    task3

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 
 const firstArg = process.argv[2];
-const secondArg = process.argv[3];
 
-console.log(firstArg + ' is ' + secondArg);
+if (firstArg === undefined) {
+  console.log('No argument');
+} else {
+  console.log(firstArg);
+}
