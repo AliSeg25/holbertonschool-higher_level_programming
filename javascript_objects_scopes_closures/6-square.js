4b7f98ea21992c2ff1c74da9b5af5fc0ebb9e268
Your Name <you@example.com>
2023-04-09 12:14:16 +0200

    task6

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+const Square1 = require('./5-square');
+
+class Square extends Square1 {
+  charPrint(c) {
+    if (c === undefined) {
+      c = "X";
+    }
+    let str = "";
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.width; j++) {
+        str += c;
+      }
+      str += "\n";
+    }
+    console.log(str);
+  }
+}
+
+module.exports = Square;
