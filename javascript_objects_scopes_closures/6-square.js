297a00c010c68368a9834133807c5f3b14912def
Your Name <you@example.com>
2023-04-09 12:16:29 +0200

    task6

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,20 +1,13 @@
 #!/usr/bin/node
-const Square1 = require('./5-square');
-
-class Square extends Square1 {
-  charPrint(c) {
-    if (c === undefined) {
-      c = "X";
-    }
-    let str = "";
-    for (let i = 0; i < this.width; i++) {
-      for (let j = 0; j < this.width; j++) {
-        str += c;
+class Square extends require('./5-square') {
+    charPrint(c) {
+      if (c === undefined) {
+        c = 'X';
       }
-      str += "\n";
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
     }
-    console.log(str);
   }
-}
-
-module.exports = Square;
+  
+  module.exports = Square;
