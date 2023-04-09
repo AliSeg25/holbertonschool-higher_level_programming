446668116c832239e4b9213dfbe56e14e390f1a5
Your Name <you@example.com>
2023-04-09 09:56:23 +0200

    task8

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -3,12 +3,12 @@
 const size = Number(process.argv[2]);
 
 if (isNaN(size)) {
-  console.log("Missing size");
+  console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
-    let row = "";
+    let row = '';
     for (let j = 0; j < size; j++) {
-      row += "X";
+      row += 'X';
     }
     console.log(row);
   }
