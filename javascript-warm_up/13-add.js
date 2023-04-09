40ac12d8c7712d89fd3b8bef7526f1d190d184d1
Your Name <you@example.com>
2023-04-09 10:47:18 +0200

    finishh

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,10 +1,3 @@
-#!/usr/bin/node
-const myObject = {
-    type: 'object',
-    value: 12
-    };
-    console.log(myObject);
-    
-    myObject.value = 89;
-    
-    console.log(myObject);
+exports.add = function add(a, b) {
+    return a + b;
+  };
