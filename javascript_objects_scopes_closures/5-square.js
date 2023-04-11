957336d09690c5b81a3958ebe4b1cdac91fec9e8
Your Name <you@example.com>
2023-04-11 08:38:58 +0200

    style

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,50 +1,49 @@
 #!/usr/bin/node
 
 class Rectangle {
-    constructor(w, h) {
-      if (w <= 0 || h <= 0) {
-      } else {
-        this.width = w;
-        this.height = h;
-      }
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+    } else {
+      this.width = w;
+      this.height = h;
     }
-  
-    print() {
-        if (!this.width || !this.height){
+  }
 
-        }else{
-            let str = '';
-            for (let i = 0; i < this.height; i++) {
-            for (let j = 0; j < this.width; j++) {
-                str += 'X';
-            }
-            console.log(str);
-            str = '';
-            }
+  print () {
+    if (!this.width || !this.height) {
+
+    } else {
+      let str = '';
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
+          str += 'X';
         }
-    }
-  
-    rotate() {
-      if (!this.width || !this.height) {
-      } else {
-        [this.width, this.height] = [this.height, this.width];
+        console.log(str);
+        str = '';
       }
     }
-  
-    double() {
-      if (!this.width || !this.height) {
-      } else {
-        this.width *= 2;
-        this.height *= 2;
-      }
+  }
+
+  rotate () {
+    if (!this.width || !this.height) {
+    } else {
+      [this.width, this.height] = [this.height, this.width];
     }
+  }
+
+  double () {
+    if (!this.width || !this.height) {
+    } else {
+      this.width *= 2;
+      this.height *= 2;
+    }
+  }
 }
-  
 
 class Square extends Rectangle {
-    constructor(size) {
-      super(size, size);
-    }
+  constructor (size) {
+    super(size, size);
+  }
 }
 
 module.exports = Square;
